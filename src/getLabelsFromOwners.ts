39a4b5c99e1123c50571a7e12ecae99530f22eda
siva273 <siva@spotdraft.com>
2022-09-01 13:56:11 +0530

    update owner name

--- src/getLabelsFromOwners.ts
+++ src/getLabelsFromOwners.ts
@@ -10,8 +10,11 @@ export async function getLabelsFromOwners(
 ): Promise<Set<Label>> {
   const labels: Set<Label> = new Set([])
   for (const owner of owners) {
+    const owner_split = owner.split("/", 2);
+    const re = "-";
+    const owner_updated = owner_split[1].replace(re, "/");
     labels.add({
-      name: `${owner}`,
+      name: `${owner_updated}`,
       // From the documentation: https://octokit.github.io/rest.js/#octokit-routes-issues-create-label
       // > The hexadecimal color code for the label, without the leading #
       // randomcolor() returns a color code with a '#' prefix, so we remove it
