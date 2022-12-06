fcb6fd14c3cbfe497968e05dbd315bc3780d5ca7
siva273 <siva@spotdraft.com>
2022-12-06 12:15:15 +0530

    Update getLabelsFromOwners.ts

--- src/getLabelsFromOwners.ts
+++ src/getLabelsFromOwners.ts
@@ -8,16 +8,16 @@ export async function getLabelsFromOwners(
   owners: Set<string>
 ): Promise<Set<Label>> {
   const labels: Set<Label> = new Set([])
-  const owner_updated: Set<string> = new Set("")
   for (const owner of owners) {
+    let owner_updated
     if (owner.includes("/")) {
       const owner_split = owner.split("/", 2);
       const re = "-";
-      const owner_updated = owner_split[1].replace(re, "/");
+      owner_updated = owner_split[1].replace(re, "/");
     }
     else {
       const re = "";
-      const owner_updated = owner.replace(re, "@");
+      owner_updated = owner.replace(re, "@");
     }
     // const owner_split = owner.split("/", 2);
     // const re = "-";
