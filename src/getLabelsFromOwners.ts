4b392fe3c92a4ffeba395ec9520ee197f4c0caf4
siva273 <siva@spotdraft.com>
2022-12-06 11:53:05 +0530

    label individual code owners also

--- src/getLabelsFromOwners.ts
+++ src/getLabelsFromOwners.ts
@@ -10,9 +10,18 @@ export async function getLabelsFromOwners(
 ): Promise<Set<Label>> {
   const labels: Set<Label> = new Set([])
   for (const owner of owners) {
-    const owner_split = owner.split("/", 2);
-    const re = "-";
-    const owner_updated = owner_split[1].replace(re, "/");
+    if (owner.includes("/")) {
+      const owner_split = owner.split("/", 2);
+      const re = "-";
+      const owner_updated = owner_split[1].replace(re, "/");
+    }
+    else {
+      const re = "";
+      const owner_updated = owner.replace(re, "@");
+    }
+    // const owner_split = owner.split("/", 2);
+    // const re = "-";
+    // const owner_updated = owner_split[1].replace(re, "/");
     labels.add({
       name: `${owner_updated}`,
       // From the documentation: https://octokit.github.io/rest.js/#octokit-routes-issues-create-label
