7c77976489851063e04ae9ba9e1015ac2aa33c9e
siva273 <siva@spotdraft.com>
2022-12-06 12:01:36 +0530

    Update getLabelsFromOwners.ts

--- src/getLabelsFromOwners.ts
+++ src/getLabelsFromOwners.ts
@@ -1,5 +1,4 @@
 import randomColor from 'randomcolor'
-const owner_updated = ""
 export interface Label {
   name: string
   color: string
@@ -14,21 +13,29 @@ export async function getLabelsFromOwners(
       const owner_split = owner.split("/", 2);
       const re = "-";
       const owner_updated = owner_split[1].replace(re, "/");
+      labels.add({
+        name: `${owner_updated}`,
+        // From the documentation: https://octokit.github.io/rest.js/#octokit-routes-issues-create-label
+        // > The hexadecimal color code for the label, without the leading #
+        // randomcolor() returns a color code with a '#' prefix, so we remove it
+        color: randomColor().substr(1)
+      })
     }
     else {
       const re = "";
       const owner_updated = owner.replace(re, "@");
+      labels.add({
+        name: `${owner_updated}`,
+        // From the documentation: https://octokit.github.io/rest.js/#octokit-routes-issues-create-label
+        // > The hexadecimal color code for the label, without the leading #
+        // randomcolor() returns a color code with a '#' prefix, so we remove it
+        color: randomColor().substr(1)
+      })
     }
     // const owner_split = owner.split("/", 2);
     // const re = "-";
     // const owner_updated = owner_split[1].replace(re, "/");
-    labels.add({
-      name: `${owner_updated}`,
-      // From the documentation: https://octokit.github.io/rest.js/#octokit-routes-issues-create-label
-      // > The hexadecimal color code for the label, without the leading #
-      // randomcolor() returns a color code with a '#' prefix, so we remove it
-      color: randomColor().substr(1)
-    })
+
   }
   return labels
 }
